7fc3ee3f5074eecc445f4accd80a3a414e1770dc
Сергей <osedchii.serega48@gmail.com>
2024-09-11 18:47:32 +0300

    client_code update

--- coffee_shop/client_code.py
+++ coffee_shop/client_code.py
@@ -4,17 +4,19 @@ from drinks import HoneyRiff
 from interfaces import Drink
 from random import choice
 
-list_of_drinks = [
-    CoffeeMulledWineWithRaspberry(),
-    GingerAndOrangeTea(),
-    HoneyRiff(),
-]
-
 
 def client_code(drink: Drink):
     print(drink.get_a_drink())
 
 
-for _ in range(10):
-    client_code(choice(list_of_drinks))
-    print("***" * 30)
+if __name__ == "__main__":
+
+    list_of_drinks = [
+        CoffeeMulledWineWithRaspberry(),
+        GingerAndOrangeTea(),
+        HoneyRiff(),
+    ]
+
+    for _ in range(10):
+        client_code(choice(list_of_drinks))
+        print("***" * 30)
